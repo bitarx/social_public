3116443d5d1e9261d690467b25c1addc0d977bee
barx <ryo238715@gmail.com>
2015-01-13 12:25:42 +0900

    tutorial

--- app/Controller/TutorialsController.php
+++ app/Controller/TutorialsController.php
@@ -1087,7 +1087,6 @@ class TutorialsController extends ApiController {
         $inviteUserId = 0;
         $where = array(
             'invite_sns_user_id' => $this->ownerId 
-        ,   'callback_flg' => 1 
         ,   'point2_flg' => 0 
         );
         $inviteUserId = $this->FriendInvite->field('user_id', $where);
